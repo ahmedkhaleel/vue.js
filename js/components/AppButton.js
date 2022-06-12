9ad1d6ae7880e6d7d4fa2f77764f8d1492b45771
ahmedkhaleel <duZhuqkyptu8barnuz>
2022-06-12 14:45:18 +0200

    Component Props

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,14 +1,29 @@
 export default {
       template:
-          `<button class="bg-gray-200 hover:bg-gray-400 border rounded py-2 px-6 mx-3 disabled:cursor-not-allowed" :disabled="processing">
+          `<button 
+              :class="{
+              'border rounded py-2 px-6 mx-3 disabled:cursor-not-allowed' : true,
+              'bg-blue-200 hover:bg-gray-400 ' : type === 'primary',
+              'bg-purple-200 hover:bg-gray-400 ' : type === 'secondary',
+              'bg-red-200 hover:bg-gray-400 ' : type === 'muted',
+              'is-loading': processing
+              }"
+              :disabled="processing"
+              >
                           <slot />
                          </button>
                        `,
-      data(){
-        return {
-          processing:false
+      props: {
+            type: {
+              type: String,
+              default: 'primary',
+            },
+        processing:{
+          type: Boolean,
+          default: false,
         }
-      }
+      },
+
 
 
 }
