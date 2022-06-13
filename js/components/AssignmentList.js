1f8861eecee61f0c181429385c9b580c567a0ac2
ahmedkhaleel <duZhuqkyptu8barnuz>
2022-06-13 19:54:37 +0200

    Parent-Child State Communication

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -4,7 +4,7 @@ export default {
   components: { Assignment },
 
   template: `
-        <section v-show="assignments.length" class="">
+        <section v-show="assignments.length">
             <h2 class="font-bold mb-2">{{ title }}</h2>
             <ul class="border border-gray-600 divide-y divide-gray-600">
                <assignment 
--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -1,52 +1,44 @@
-import AssignmentList from './AssignmentList.js';
+import AssignmentList from "./AssignmentList.js";
+import AssignmentCreate from "./AssignmentCreate.js";
 
 export default {
-  components: {AssignmentList},
+  components: { AssignmentList, AssignmentCreate },
 
   template: `
         <section class="space-y-6">
             <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
             <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
-            <form @submit.prevent="add">
-            <div class="border border-gray-600 text-black">
-            <input v-model="newAssignment" placeholder="add Assignment" class="p-2">
-            <button type="submit" class=" bg-white p-2 border-l">Add</button>
-             </div>
-            </form>
+            
+            <assignment-create @add="add"></assignment-create> 
         </section>
-        
-        
     `,
 
   data() {
     return {
       assignments: [
-        {name: 'Finish project', complete: false, id: 1},
-        {name: 'Read Chapter 4', complete: false, id: 2},
-        {name: 'Turn in Homework', complete: false, id: 3}
+        { name: 'Finish project', complete: false, id: 1 },
+        { name: 'Read Chapter 4', complete: false, id: 2 },
+        { name: 'Turn in Homework', complete: false, id: 3 },
       ],
-      newAssignment: ''
-    };
+    }
   },
 
   computed: {
     filters() {
       return {
-        inProgress: this.assignments.filter(assignment => !assignment.complete),
+        inProgress: this.assignments.filter(assignment => ! assignment.complete),
         completed: this.assignments.filter(assignment => assignment.complete)
       };
     }
   },
 
   methods: {
-    add() {
+    add(name) {
       this.assignments.push({
-        name: this.newAssignment,
-        complete: false,
-        id: this.assignments.length + 1,// this is a unique ide
+        name: name,
+        completed: false,
+        id: this.assignments.length + 1
       });
-
-      this.newAssignment = '';
     }
   }
 }
